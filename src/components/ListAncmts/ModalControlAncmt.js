22ee0ac9288d3ad5a94160264480de14e59a2392
teneons <teneons@gmail.com>
2020-11-17 20:50:04 +0200

    made modal control for ancmt

---
+++ src/components/ListAncmts/ModalControlAncmt.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+export default class ModalControlAncmt extends Component {
+
+  render() {
+
+    return(
+      <div className="modal fade" id="modalControl" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden={false}>
+      <div className="modal-dialog modal-dialog-centered">
+        <form className="modal-content form-group">
+          <div className="modal-header">
+            <input className="modal-title form-control fw-bold text-secondary" ref={(data)=> this.txtTitle = data} minLength={2} maxLength={30} type='text' placeholder='Title announcement' id="exampleModalLabel" required />
+            <button type="button" className="btn-close" data-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div className="modal-body">
+            <span className='card-subtitle text-muted right-align'>
+              <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-calendar2-event" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z"/>
+                <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4zM11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
+              </svg>
+            <span className='fw-bold' ref={(data)=> this.txtDate = data}>2000</span></span>
+            <textarea className="form-control mt-2" ref={(data)=> this.txtDescription = data} minLength={2} maxLength={295} rows="3" placeholder='Description announcement' required />
+            <input className='form-control col-12 mt-2 inputContact' ref={(data)=> this.txtContact = data} minLength={2} maxLength={40} type='text' placeholder='Contact data' required />
+          </div>
+          <div className="modal-footer d-flex justify-content-center">
+            <button type="button" className="btn btn-danger">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+              </svg>
+            </button>
+            <button type="button" className="btn btn-dark">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+              </svg>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+    )
+  }
+}

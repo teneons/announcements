56950fdf48d59e3722d22a928c6eab7702da4622
teneons <teneons@gmail.com>
2020-11-17 16:40:00 +0200

    out ancmts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,10 @@
+import React, { Component } from 'react';
 import Header from '../Header/Header';
-import ListAncmts from '../ListAncmts/ListAncmts'
+import ListAncmts from '../ListAncmts/ListAncmts';
 
-function App() {
+
+export default class App extends Component {
+  render() {
   return (
     <div className="App">
       <Header />
@@ -9,5 +12,4 @@ function App() {
     </div>
   );
 }
-
-export default App;
+}
--- src/components/ListAncmts/ListAncmts.js
+++ src/components/ListAncmts/ListAncmts.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class ListAncmt extends Component {
+class ListAncmt extends Component {
 
   render() {
 
     return (
       <div className='container mt-4 col-sm-12 col-md-8 col-lg-5 shadow p-3'>
         <ul className="list-group">
-          <li className="list-group-item">Cras justo odio</li>
-          <li className="list-group-item">Dapibus ac facilisis in</li>
-          <li className="list-group-item">Morbi leo risus</li>
-          <li className="list-group-item">Porta ac consectetur ac</li>
-          <li className="list-group-item">Vestibulum at eros</li>
+          {this.props.ancmtsStore.map((ancmt) =>
+            <li className="list-group-item" key={ancmt.id}>{ancmt.title}</li>
+          )}
         </ul>
       </div>
     )
   }
 }
+
+export default connect(
+  state => ({
+    ancmtsStore: state.ancmts
+  }),
+  dispatch => ({})
+)(ListAncmt)

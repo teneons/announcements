d2120e74034d370a94f0e0b9d161476a15764055
teneons <teneons@gmail.com>
2020-11-19 20:45:55 +0200

    made search

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,13 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import BtnAdd from '../BtnAdd/BtnAdd';
+import { connect } from 'react-redux';
 
-export default class Header extends Component {
+class Header extends Component {
+
+  getSearchTxt() {
+    return this.props.searchAncmt(this.srchTxt.value)
+  }
 
   render() {
   return(
@@ -15,10 +20,10 @@ export default class Header extends Component {
     <form className="row container d-flex justify-content-start justify-content-md-evenly col-12 col-sm-7 col-md-6 col-lg-5 col-xl-4">
         <div className='row'>
           <div className='col-10 mt-2'>
-            <input className="form-control" type="search" placeholder="Search to announcements" aria-label="Search" />
+            <input className="form-control" ref={(data) =>this.srchTxt = data} type="search" placeholder="Search to announcements" aria-label="Search" />
           </div>
         <div className='col-1'>
-          <button className="btn btn-outline-warning my-2 font-weight-bold text-uppercase" type="submit">
+          <button type="button" onClick={this.getSearchTxt.bind(this)}  className="btn btn-outline-warning my-2 font-weight-bold text-uppercase" >
             <svg width="1.4em" height="1.4em" viewBox="0 0 16 16" className="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
               <path fillRule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
               <path fillRule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
@@ -31,3 +36,12 @@ export default class Header extends Component {
   )
   }
 }
+
+export default connect(
+  state => ({}),
+  dispatch => ({
+    searchAncmt: (searchTxt) => {
+      dispatch({type: 'SEARCH_ANCMT', payload: searchTxt})
+    }
+  })
+)(Header)
--- src/redusers/ancmts.js
+++ src/redusers/ancmts.js
@@ -6,7 +6,7 @@ export default function ancmts(state = initState, action) {
     case 'ADD_NEW_ANCMT': return [...state, action.payload];
     case 'OPEN_ANCMT': return state;
     case 'REMOVE_ANCMT': return state.slice(0, state.findIndex((item)=>item.id === action.payload)).concat(state.slice(state.findIndex((item)=>item.id === action.payload)+1));
-    case 'SEARCH_ANCMT': return state;
+    case 'SEARCH_ANCMT': return state.filter(item => item.title.includes(action.payload));
     default: return state
   }
 

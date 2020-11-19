7e699fd268da3d0fbcdbbd8f2b99e08003541124
teneons <teneons@gmail.com>
2020-11-19 18:25:39 +0200

    added processing remove, search ancmt

--- src/redusers/ancmts.js
+++ src/redusers/ancmts.js
@@ -1,10 +1,12 @@
-const initState = [];
+const initState = []
 
 export default function ancmts(state = initState, action) {
 
   switch(action.type) {
     case 'ADD_NEW_ANCMT': return [...state, action.payload];
     case 'OPEN_ANCMT': return state;
+    case 'REMOVE_ANCMT': return state.slice(0, state.findIndex((item)=>item.id === action.payload)).concat(state.slice(id+1));
+    case 'SEARCH_ANCMT': return state;
     default: return state
   }
 

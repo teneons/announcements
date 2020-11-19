1b641a8a3af0a26d6c5995c7eb71f305e1e0b6c6
teneons <teneons@gmail.com>
2020-11-19 17:48:44 +0200

    remake open ancmt (beta)

--- src/components/ListAncmts/ModalOpenAncmt.js
+++ src/components/ListAncmts/ModalOpenAncmt.js
@@ -3,20 +3,50 @@ import { connect } from 'react-redux';
 
 
 class ModalOpenAncmt extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       id: null,
+       title: null,
+       date: null,
+       description: null,
+       contact: null
+    }
+  }
+  
 
-  componentDidUpdate() {
-    this.idByProps()
+  componentDidUpdate(prevPrps) {
+    if(this.props.propsID !== prevPrps.propsID){
+      this.props.getIdByProps(this.props.propsID)
+    
+      this.props.ancmtState.map((item)=>{
+        this.setState({
+          id: item.id,
+          title: item.title,
+          date: item.date,
+          description: item.description,
+          contact: item.contact
+        })
+      })
+    }
   }
 
-  idByProps() {
-    this.props.getIdByProps(this.props.propsID)
+  componentWillUnmount() {
+    this.setState({
+      id: null,
+      title: null,
+      date: null,
+      description: null,
+      contact: null
+    })
   }
 
   render() {
 
-    return (
-      <div className="modal fade" id="modalOpen" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        {this.props.ancmtState.map((item)=> {
+    const outModal = this.props.ancmtState.map((item) => {
+      return(
+      <div className="modal fade" id="modalOpen" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" key={item.id}>
         <div className="modal-dialog modal-dialog-centered">
           <div className="modal-content">
             <div className="modal-header bg-warning">
@@ -59,7 +89,54 @@ class ModalOpenAncmt extends Component {
             </div>
           </div>
         </div>
-        })}
+      </div>
+      )
+    })
+
+    return (
+     <div className="modal fade" id="modalOpen" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" key={this.state.id}>
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header bg-warning">
+              <h5 className="modal-title fs-4 fw-bold" id="exampleModalLabel">
+                <svg width="0.8em" height="0.8em" viewBox="0 0 16 16" className="bi bi-info-square mr-1 text-white" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                  <path fillRule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                  <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z" />
+                  <circle cx="8" cy="4.5" r="1" />
+                </svg>
+                <span className='text-white'>{this.state.title}</span>
+              </h5>
+              <button type="button" className="btn-close" data-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+
+              <p >
+                <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-calendar2-event mr-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z" />
+                  <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4zM11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
+                </svg>
+                <span className='text-muted'>{this.state.date}</span>
+              </p>
+
+              <p>
+                <svg width="1.3em" height="1.3em" viewBox="0 0 16 16" className="bi bi-receipt mr-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" d="M1.92.506a.5.5 0 0 1 .434.14L3 1.293l.646-.647a.5.5 0 0 1 .708 0L5 1.293l.646-.647a.5.5 0 0 1 .708 0L7 1.293l.646-.647a.5.5 0 0 1 .708 0L9 1.293l.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .801.13l.5 1A.5.5 0 0 1 15 2v12a.5.5 0 0 1-.053.224l-.5 1a.5.5 0 0 1-.8.13L13 14.707l-.646.647a.5.5 0 0 1-.708 0L11 14.707l-.646.647a.5.5 0 0 1-.708 0L9 14.707l-.646.647a.5.5 0 0 1-.708 0L7 14.707l-.646.647a.5.5 0 0 1-.708 0L5 14.707l-.646.647a.5.5 0 0 1-.708 0L3 14.707l-.646.647a.5.5 0 0 1-.801-.13l-.5-1A.5.5 0 0 1 1 14V2a.5.5 0 0 1 .053-.224l.5-1a.5.5 0 0 1 .367-.27zm.217 1.338L2 2.118v11.764l.137.274.51-.51a.5.5 0 0 1 .707 0l.646.647.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.509.509.137-.274V2.118l-.137-.274-.51.51a.5.5 0 0 1-.707 0L12 1.707l-.646.647a.5.5 0 0 1-.708 0L10 1.707l-.646.647a.5.5 0 0 1-.708 0L8 1.707l-.646.647a.5.5 0 0 1-.708 0L6 1.707l-.646.647a.5.5 0 0 1-.708 0L4 1.707l-.646.647a.5.5 0 0 1-.708 0l-.509-.51z" />
+                  <path fillRule="evenodd" d="M3 4.5a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm8-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z" />
+                </svg>
+                <span>{this.state.description}</span>
+              </p>
+
+              <p>
+                <svg width="1.2em" height="1.2em" viewBox="0 0 16 16" className="bi bi-person-square mr-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                  <path fillRule="evenodd" d="M2 15v-1c0-1 1-4 6-4s6 3 6 4v1H2zm6-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                </svg>
+                <span className='fw-bold'>{this.state.contact}</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
@@ -72,7 +149,7 @@ export default connect(
   }),
   dispatch => ({
     getIdByProps: (id) => {
-      dispatch({type: 'OPEN_ANCMT', payload: id})
+      dispatch({ type: 'OPEN_ANCMT', payload: id })
     }
   })
 )(ModalOpenAncmt)
--- src/redusers/ancmts.js
+++ src/redusers/ancmts.js
@@ -5,9 +5,7 @@ export default function ancmts(state = initState, action) {
   if(action.type === 'ADD_NEW_ANCMT') {
     return [...state, action.payload]
   } else if(action.type === 'OPEN_ANCMT') {
-    console.log(action.payload)
-    const obj = state.filter(el => el.id === action.payload)
-    console.log(obj)
+    //const obj = state.filter(el => el.id === action.payload)
     return state
   }
 

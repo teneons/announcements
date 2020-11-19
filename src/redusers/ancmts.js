6684a26b5d1e017d26c004a5db3e4079375c9b12
teneons <teneons@gmail.com>
2020-11-19 18:04:06 +0200

    remake redux processing actions

--- src/redusers/ancmts.js
+++ src/redusers/ancmts.js
@@ -2,12 +2,10 @@ const initState = [];
 
 export default function ancmts(state = initState, action) {
 
-  if(action.type === 'ADD_NEW_ANCMT') {
-    return [...state, action.payload]
-  } else if(action.type === 'OPEN_ANCMT') {
-    //const obj = state.filter(el => el.id === action.payload)
-    return state
+  switch(action.type) {
+    case 'ADD_NEW_ANCMT': return [...state, action.payload];
+    case 'OPEN_ANCMT': return state;
+    default: return state
   }
 
-  return state
 }
